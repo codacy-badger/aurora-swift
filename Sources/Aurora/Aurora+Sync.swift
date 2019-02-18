6adf9fc3b8d320dd9df5053be8178a58c776881e
Alex Young <alex.buzov@icloud.com>
2019-02-18 12:38:01 -0500

    Events fix (#25)
    * Update Aurora+Sync.swift
    
    * Update Aurora+Sync.swift

--- Sources/Aurora/Aurora+Sync.swift
+++ Sources/Aurora/Aurora+Sync.swift
@@ -16,7 +16,7 @@ extension Aurora {
     func sync(event: [String: Any]) {
         guard let name = event[Attribute.name.rawValue] as? String else { return }
         guard let type = event[Attribute.type.rawValue] as? String else { return }
-        let payload = event[Attribute.type.rawValue] as? String
+        let payload = event[Attribute.payload.rawValue]
         let event = Event(name: name, type: type, payload: payload)
         delegates.forEach { $0.didReceive(event: event) }
     }
